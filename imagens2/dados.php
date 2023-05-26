7488479df283304b47af8d00f5a07c63ba6a4058
manuzita1005 <132273489+manuzita1005@users.noreply.github.com>
2023-05-25 22:18:56 -0300

    Create dados.php
    criei o array de dados

---
+++ imagens2/dados.php
@@ -0,0 +1,51 @@
+<?php
+
+$servicos = [   
+    [        
+        "nome" => "Unhas",
+        "descricao" => "Alongamento,banho de Acrilico, Banho de Gel, Decoração, Esmaltação, Manicure, Pedicure",
+        "foto" => "imagens2/2.jpg",
+    ],
+    [        
+        "nome" => "Massagem",
+        "descricao" => "Massagem Relaxante, Modeladora e muito mais",
+        "foto" => "imagens2/1.jpg",
+    ],
+    [        
+        "nome" => "Maquiagem",
+        "descricao" => "Maquiagem by Natura UNA, Maquiagem Básica e outros tipos",
+        "foto" => "imagens2/3.jpg",
+    ],
+    [        
+        "nome" => "Estética",
+        "descricao" => "Limpeza de pele tradicional,Limpeza de pele com hidratação",
+        "foto" => "imagens2/4.jpg",
+    ],
+    [        
+        "nome" => "Depilação masculina",
+        "descricao" => "Pernas Masc, Braços Masc e muito mais",
+        "foto" => "imagens2/5.jpg",
+    ],
+    [        
+        "nome" => "Depilação",
+        "descricao" => "Virilha Íntima, Limpeza de Sobrancelhas e outros tipos",
+        "foto" => "imagens2/6.jpg",
+    ],
+    [        
+        "nome" => "Cabelos",
+        "descricao" => "Escova Cabelos Curtos, Escova Cabelos Médios e Longos e outros tipos",
+        "foto" => "imagens2/7.jpg",
+    ],
+    [        
+        "nome" => "Barbearia Corleone",
+        "descricao" => "Corte tradicional ou degrade MASC, Barba e outros tipos",
+        "foto" => "imagens2/8.jpg",
+    ],
+    [        
+        "nome" => "Design de sobrancelhas",
+        "descricao" => "O design perfeito que complementa o seu rosto",
+        "foto" => "imagens2/9.jpg",
+    ],
+     
+       
+];
